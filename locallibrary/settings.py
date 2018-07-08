4fb8dfc493b6016efee8a12686675f0b6136261c
Wechuli <wechulipaul@yahoo.com>
2018-07-08 09:27:53 +0300

    Update settings.py
    changed db engine

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -83,19 +83,7 @@ DATABASES = {
     }
 }
 
-"""
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': '',
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',
-       # 'HOST': '35.193.25.158',
-        'PORT': '5432',
-    }
-}
-"""
+
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
